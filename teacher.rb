849967e19bc30bd5f8c4322ed7c283434acc9169
tsheporamantso <tshepo.ramantso@outlook.com>
2023-11-06 14:34:02 +0200

    Extended constructor with specialization instance variable

--- teacher.rb
+++ teacher.rb
@@ -1,4 +1,8 @@
 require './person'
 
 class Teacher < Person
+  def initilaize(specialization, age, name: 'Unknown', parent_permission: true)
+    super(age, name: name, parent_permission: parent_permission)
+    @specialization = specialization
+  end
 end
