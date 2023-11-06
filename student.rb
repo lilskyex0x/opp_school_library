952ffcb1974912c93bf8d437069d7248b9aa8141
tsheporamantso <tshepo.ramantso@outlook.com>
2023-11-06 14:19:21 +0200

    Extended constructor by classroom instance variable

--- student.rb
+++ student.rb
@@ -1,4 +1,8 @@
 require './person'
 
 class Student < Person
+  def initilaize(classroom, age, name: 'Unknown', parent_permission: true)
+    super(age, name: name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
 end
