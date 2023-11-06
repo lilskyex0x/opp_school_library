e0c50b4546c0aa85ee7582e54a912d17cdc1b01d
tsheporamantso <tshepo.ramantso@outlook.com>
2023-11-06 14:03:15 +0200

    Defined of_age method with conditional statement

--- person.rb
+++ person.rb
@@ -2,10 +2,16 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  def initilaize (age, name: "Unknown", parent_permission: true)
+  def initilaize(age, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @age = age
-    @name = name 
+    @name = name
     @parent_permission = parent_permission
+  end
+
+  def of_age?
+    return true if age >= 18
+
+    false
   end
 end
