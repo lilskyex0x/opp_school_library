6aad6604aa3ef57c67293f7a80d796d51eff4bec
tsheporamantso <tshepo.ramantso@outlook.com>
2023-11-07 14:43:13 +0200

    Initialized decorator,capitalized and trimmer variables and add test case

--- person.rb
+++ person.rb
@@ -1,4 +1,7 @@
 require './nameable'
+require './decorator'
+require './capitalize_decorator'
+require './trimmer_decorator'
 
 class Person < Nameable
   attr_reader :id
@@ -26,3 +29,14 @@ class Person < Nameable
     @age >= 18
   end
 end
+
+person = Person.new(22, name: 'maximilianus')
+person.correct_name
+
+capitalized_person = CapitalizeDecorator.new(person)
+
+puts capitalized_person.correct_name
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+
+puts capitalized_trimmed_person.correct_name
