32030fa16b964f99a32679c2b5b58db5bf05e5bd
tsheporamantso <tshepo.ramantso@outlook.com>
2023-11-09 10:36:01 +0200

    Initialized people instance variable and def method that loops through people and check instance of teacher or student and return output

--- app.rb
+++ app.rb
@@ -1,10 +1,13 @@
 require './book'
+require './student'
+require './teacher'
 
 class App
-  attr_accessor :books
+  attr_accessor :books, :people
 
   def initialize
     @books = []
+    @people = []
   end
 
   def book_list
@@ -13,6 +16,20 @@ class App
     else
       @book.each_with_index do |book, index|
         puts "#{index} - Title: #{book.title.capitalize}, Author: #{book.author.capitalize}"
+      end
+    end
+  end
+
+  def people_list
+    if people.empty?
+      puts 'There is no people in the list'
+    else
+      @people.each_with_index do |person, index|
+        if person.instance_of?(Teacher)
+          puts "#{index} [Teacher] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+        elsif person.instance_of?(Student)
+          puts "#{index} [Student] Name: #{person.name} ID: #{person.id}, Age: #{person.age}"
+        end
       end
     end
   end
