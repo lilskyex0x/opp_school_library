e8591c69afc5dd2c218b80f9f1289f94a0d6812d
tsheporamantso <tshepo.ramantso@outlook.com>
2023-11-09 08:55:32 +0200

    Initialized book instance variable and define book_list method with conditional statement and loop through book class to create book list

--- app.rb
+++ app.rb
@@ -1,2 +1,19 @@
+require './book'
+
 class App
+  attr_accessor :books
+
+  def initialize
+    @books = []
+  end
+
+  def book_list
+    if @books.empty?
+      puts 'There is no book in the list'
+    else
+      @book.each_with_index do |book, index|
+        puts "#{index} - Title: #{book.title.capitalize}, Author: #{book.author.capitalize}"
+      end
+    end
+  end
 end
