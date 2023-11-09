e4cec64b1ff0cdc1484b2116b8aefcd4030677eb
tsheporamantso <tshepo.ramantso@outlook.com>
2023-11-09 11:19:23 +0200

    Define method teacher_create that prompts user to successfully create an object of a teacher

--- app.rb
+++ app.rb
@@ -46,4 +46,16 @@ class App
       puts 'Invalid entry'
     end
   end
+
+  def teacher_create
+    puts 'Age:'
+    age = gets.chomp
+    puts 'Name:'
+    name = gets.chomp
+    puts 'Specialization:'
+    specialization = gets.chomp
+    parent_permission = true
+    @people.push Teacher.new(age: age, name: name, specialization: specialization, parent_permission: parent_permission)
+    puts 'Person created successfully!'
+  end
 end
