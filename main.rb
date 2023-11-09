85b9c61c139cec963d0cb7c06a2e01d8a8a0bce2
tsheporamantso <tshepo.ramantso@outlook.com>
2023-11-09 14:59:30 +0200

    Inside loop do, add condition to break when option 7 is selected

--- main.rb
+++ main.rb
@@ -9,6 +9,11 @@ def main
   loop do
     display_menu
     option = gets.chomp
+
+    if option == '7'
+        puts 'Thank you for using this app!'
+        break
+    end
     options(option, app)
   end
 end
