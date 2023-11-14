649c873a64ed8e6db4caae0cefeca611449aa042
tsheporamantso <tshepo.ramantso@outlook.com>
2023-11-14 10:51:56 +0200

    Define methods to write and read files

--- preserve_data.rb
+++ preserve_data.rb
@@ -1,1 +1,16 @@
 require 'json'
+
+def write_file(obj, filename)
+  data = obj.map(&:to_h)
+  data_json = JSON.generate(data)
+  File.write(filename, data_json, mode: 'w')
+end
+
+def read_file(filename)
+  if File.exist?(filename) and File.size(filename) != 0
+    data = File.read(filename)
+    JSON.parse(data)
+  else
+    []
+  end
+end
