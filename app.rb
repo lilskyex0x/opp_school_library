89af09da177820b29469f270db05b2c8591f3884
tsheporamantso <tshepo.ramantso@outlook.com>
2023-11-15 10:14:01 +0200

    Modified book_list method

--- app.rb
+++ app.rb
@@ -18,8 +18,9 @@ class App
       puts 'There is no book in the list'
     else
       @books.each_with_index do |book, index|
-        puts "#{index} - Title: #{book['title'].capitalize},
-        Author: #{book['author'].capitalize} Rentals: #{book['rentals']}"
+        rentals_str = book['rentals'].join(', ')
+        puts "#{index} - Title: #{book['title'].capitalize}, " + 
+        "Author: #{book['author'].capitalize} Rentals: #{book['rentals']}"
       end
     end
   end
@@ -30,9 +31,9 @@ class App
       puts 'There is no people in the list'
     else
       @people.each_with_index do |person, index|
-        if person.instance of?(Teacher)
+        if person.instance_of?(Teacher)
           puts "#{index} [Teacher] Name: #{person['name']}, ID: #{person[id]}, Age: #{person[age]}"
-        elsif person.instance of?(Student)
+        elsif person.instance_of?(Student)
           puts "#{index} [Student] Name: #{person['name']} ID: #{person['id']}, Age: #{person['age']}"
         end
       end
